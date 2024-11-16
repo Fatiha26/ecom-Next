f84a0d716e91038a3a68eab8d06f884dbdb0ec4e
Fatiha26 <f6lamia@gmail.com>
2024-11-16 23:24:47 +0600

    nav completed

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -5,8 +6,11 @@ const Navbar = () => {
     <div className='hidden lg:block'>
         <div className='container'>
             <div className='flex w-fit gap-10 mx-auto font-medium py-4 text-blackish'>
-                
-
+                <Link href='#' className='navbar__link relative'>Home</Link>
+                <Link href='#' className='navbar__link relative'>Categories</Link>
+                <Link href='#' className='navbar__link relative'>Blogs</Link>
+                <Link href='#' className='navbar__link relative'>Offers</Link>
+                <Link href='#' className='navbar__link relative'>Career</Link>
             </div>
 
         </div>
